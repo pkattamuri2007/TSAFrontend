da4165646e97d2d3a8a055d3095c608f46d1b8a7
Sahil Reddy <sahilreddy@sahils-air.lan>
2023-09-10 18:24:57 -0400

    begun adding percentages

--- tsa-frontend/src/App.js
+++ tsa-frontend/src/App.js
@@ -5,85 +5,118 @@ import data from "./data.json";
 import diseaseData from "./data2.json";
 import { config } from "dotenv";
 import OpenAI from "openai";
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import * as calc from "./Components/PercentCalculator";
+// OPEN AI CONFIGURATION
 config();
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_API_KEY,
   dangerouslyAllowBrowser: true, // This is also the default, can be omitted
 });
+//DEFINING USER INPUTTED ARRAY
 const zeroArray = new Array(406).fill(0);
+const freqArray = new Array(406).fill(0);
+
 function App() {
+  //LISTS ALL SYMPTOMS
   const [symptomList, setSymptomList] = useState([]);
+  //LISTS ALL DISEASES
   const [finalDiseases, setFinalDiseases] = useState([]);
+  //USED TO TRIGGER USE EFFECT
   const [whenSubmit, setWhenSubmit] = useState(true);
+
+  //TEMP VARIABLE
   var diseaseNames = [];
+  //CHAT GPT CALL
   useEffect(() => {
-    if(finalDiseases.length > 0) {
-    console.log(finalDiseases);
-    var chatInput =
-    "Write a 2 sentence summary about the following disease:" +
-    finalDiseases[0][0].toString();
-  console.log(chatInput);
-  openai.chat.completions
-    .create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: chatInput }],
-    })
-    .then((response) => {
-      console.log(response.choices[0].message.content)
-      var tempFinal = finalDiseases;
-      tempFinal[0][2] = response.choices[0].message.content;
-      console.log(tempFinal);
-      setFinalDiseases(tempFinal.slice(0,5));
-    });
+    if (finalDiseases.length > 0) {
+      console.log(finalDiseases);
+
+      var chatInput =
+        "Write a two sentence description for each of the following diseases.Include a exclamation mark as a divider between each description. " 
+for (let i = 0; i < finalDiseases.length; i++) {
+  var tempStr = finalDiseases[i][0].replace(/\s/g, '');
+  chatInput +=  tempStr + ". "  
+}
+      
+      console.log(chatInput);
+      openai.chat.completions
+        .create({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: chatInput }],
+        })
+        .then((response) => {
+          console.log(response.choices[0].message.content);
+          var tempFinal = finalDiseases;
+          var responses = response.choices[0].message.content.split("!");
+          console.log(tempFinal);
+          console.log(finalDiseases);
+          console.log(responses)
+         
+          for (let i = 0; i<finalDiseases.length; i++) {
+            tempFinal[i][2] = responses[i];
+          }
+         
+          console.log(tempFinal);
+          setFinalDiseases(tempFinal.slice(0, 5));
+        });
     }
   }, [whenSubmit]);
-  
+  //WHEN SUBMIT BUTTON IS CLICKED
   const onSubmitHandler = (newData) => {
     var bestMatch = 0;
-    var bestMatches = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
+    var bestMatches = [0, 0, 0, 0, 0];
+    var symptomFreq = [];
     for (let i = 0; i < diseaseData.length; i++) {
       for (let j = 0; j < diseaseData[i].symptoms.length; j++) {
         if (zeroArray[j] === diseaseData[i].symptoms[j] && zeroArray[j] === 1) {
           bestMatch++;
+          symptomFreq.push(data[j].freq);
         }
       }
       if (bestMatch > bestMatches[4]) {
         console.log(bestMatch);
 
-        diseaseNames.unshift([diseaseData[i].disease, bestMatch]);
+        diseaseNames.unshift([
+          diseaseData[i].disease,
+          bestMatch,
+          "",
+          symptomFreq,
+        ]);
         bestMatches.unshift(bestMatch);
       }
-
       bestMatch = 0;
+      symptomFreq = [];
     }
     setFinalDiseases(diseaseNames);
     setWhenSubmit(!whenSubmit);
-
   };
   const dataSubmitHandler = (newData) => {
     setSymptomList([...symptomList, newData[0]]);
-    console.log(symptomList);
     zeroArray.splice(parseInt(newData[1]), 1, 1);
-    console.log(zeroArray);
+    console.log(data[0]);
+    console.log(parseInt(newData[1]));
+    freqArray.splice(parseInt(newData[1]), 1, data[parseInt(newData[1])].freq);
   };
-  console.log(finalDiseases);
+
   finalDiseases.sort(function (a, b) {
     return b[1] - a[1];
   });
-  console.log(finalDiseases);
 
+  var symptomChance = calc.percentCalculator(finalDiseases, symptomList);
   return (
     <div className="App">
-      <h1>{process.env.REACT_APP_SECRET_CODE}</h1>
       <SearchBar
-        placeholder="Enter Your Symptom..."
+        placeholder="Enter Your Symptoms..."
         data={data}
         onDataSubmit={dataSubmitHandler}
       />
-      <div className="App-header">Selected Symptom</div>
-      {console.log(symptomList)}
+      <div className="App-header">Selected Symptoms</div>
+
       {symptomList.map((data, id) => {
         return (
           <div key={id}>
@@ -95,15 +128,29 @@ function App() {
         Submit
       </button>
       <div>
-        Predicted Disease:{" "}
         {finalDiseases.slice(0, 5).map((data, id) => {
           return (
-            <div key={id}>
-              
-               <p>{data[0]} {data[1]} {data[2]}</p>
-                {console.log(data)}
-              
-            </div>
+            <Card key={id} sx={{ minWidth: 400, maxWidth: 400, mt: 3 }}>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Predicted Disease:
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {data[0]}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  # of Matching Symptoms: {data[1]}
+                </Typography>
+                <Typography variant="body2">{data[2]}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">{symptomChance[id]}%</Button>
+              </CardActions>
+            </Card>
           );
         })}
       </div>
---
+++ tsa-frontend/src/Components/PercentCalculator.js
@@ -0,0 +1,48 @@
+export const percentCalculator = (diseaseArray, selectedSymptoms) => {
+   var percentProbablity = [];
+    for (var i = 0; i < diseaseArray.length; i++){
+        var O = diseaseArray[i][1]/selectedSymptoms.length;
+        O = 50 * (O ** 2.5);
+        var counter = 0;
+        for (var j = 0; j < diseaseArray[i][3].length; j++){
+            console.log(diseaseArray[i][3][j]-4.7, 'difference');
+            counter += diseaseArray[i][3][j] - 4.7;
+    }
+    var C = 0
+    counter = counter / (diseaseArray[i][3].length);
+    console.log(counter, 'mad');
+    if ( counter <= 1.07) {
+        C = 20
+    }
+    if (1.07 < counter && counter <= 5.54) {
+        C = 18
+    }
+    if (5.54 < counter && counter <= 10.01) {
+        C = 16
+    }
+    if (10.01 < counter && counter <= 14.48) {
+        C = 14
+    }
+    if (14.48 < counter && counter <= 18.95) {
+        C = 12
+    }
+    if (18.95 < counter && counter <= 23.42) {
+        C = 10
+    }
+    if (23.42 < counter && counter <= 27.89) {
+        C = 8
+    }
+    if (27.89 < counter && counter <= 32.36) {
+        C = 6
+    }
+    if (32.36 < counter && counter <= 26.83) {
+        C = 4
+    }
+    if (36.83 < counter) {
+        C = 2
+    }
+var combinedTotal = Math.round(100*(O + C))/100;
+percentProbablity.push(combinedTotal);
+}
+return percentProbablity;
+}
