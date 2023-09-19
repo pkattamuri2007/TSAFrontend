84a2dc583ba286a1fb9a17ebac957e0f8977f5f9
Sahil Reddy <sahilreddy@sahils-air.lan>
2023-09-18 22:07:24 -0400

    react router

---
+++ tsa-frontend/src/Calc.js
@@ -0,0 +1,102 @@
+import SearchBar from "./Components/Searchbar";
+import Logo from "./Components/Logo";
+import data from "./data/data.json";
+import Alert from "@mui/material/Alert";
+import Symptom from "./Components/Symptom";
+import diseaseData from "./data/data2.json";
+import Disease from "./Components/Disease";
+import { chatGptCall } from "./helper/chatGptCall";
+import * as calc from "./helper/PercentCalculator";
+import { bestMatchAlgo } from "./helper/bestMatchAlgo";
+import { inputArray } from "./helper/inputArray";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const zeroArray = new Array(406).fill(0);
+const freqArray = new Array(406).fill(0);
+export default function Calc() {
+  const [isLoading, setIsLoading] = useState(false);
+  //LISTS ALL SYMPTOMS
+  const [symptomList, setSymptomList] = useState([]);
+  //LISTS ALL DISEASES
+  const [finalDiseases, setFinalDiseases] = useState([]);
+  //USED TO TRIGGER USE EFFECT
+  const [whenSubmit, setWhenSubmit] = useState(true);
+  const [description, setDescription] = useState([]);
+
+  //TEMP VARIABLE
+  var diseaseNames = [];
+
+  //CHAT GPT CALL
+  useEffect(() => {
+    chatGptCall(finalDiseases, setIsLoading, setDescription);
+  }, [whenSubmit]);
+  useEffect(() => {
+    console.log(description);
+  }, [description]);
+
+  //WHEN SUBMIT BUTTON IS CLICKED
+  const onSubmitHandler = (newData) => {
+    bestMatchAlgo(
+      diseaseData,
+      zeroArray,
+      data,
+      diseaseNames,
+      setFinalDiseases,
+      setWhenSubmit,
+      whenSubmit
+    );
+  };
+  const dataSubmitHandler = (newData) => {
+    inputArray(
+      setSymptomList,
+      newData,
+      symptomList,
+      zeroArray,
+      freqArray,
+      data
+    );
+  };
+
+  var symptomChance = calc.percentCalculator(finalDiseases, symptomList);
+  finalDiseases.sort(function (a, b) {
+    return (
+      symptomChance[finalDiseases.indexOf(b)] -
+      symptomChance[finalDiseases.indexOf(a)]
+    );
+  });
+  return (
+    <div className="App">
+      <SearchBar
+        placeholder="Enter Your Symptoms..."
+        data={data}
+        onDataSubmit={dataSubmitHandler}
+      />
+      {symptomList.length < 3 ? (
+        <Alert sx={{ my: 2 }} severity="warning">
+          Enter more symptoms for an accurate result
+        </Alert>
+      ) : null}
+      <div className="App-header">Selected Symptoms</div>
+
+      {symptomList.map((data, id) => {
+        return <Symptom data={data} id={id} />;
+      })}
+      <button className="button-9" onClick={onSubmitHandler}>
+        Submit
+      </button>
+      <div>
+        {finalDiseases.map((data, id) => {
+          return (
+            <Disease
+              diseaseInfo={data}
+              id={id}
+              symptomChance={symptomChance}
+              isLoading={isLoading}
+              description={description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ tsa-frontend/src/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Home() {
+  return <Link to="/calculate">Calculate</Link>;
+}
